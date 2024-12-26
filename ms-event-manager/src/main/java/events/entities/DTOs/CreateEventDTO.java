0f9e9a2b38f9cff3de94012a1d2e1166bef87588
gustavodm2 <gmerlugodenardi@gmail.com>
2024-12-26 18:54:07 -0300

    refactor: using lombok methods in CreateEventDTO entity

--- ms-event-manager/src/main/java/events/entities/DTOs/CreateEventDTO.java
+++ ms-event-manager/src/main/java/events/entities/DTOs/CreateEventDTO.java
@@ -6,27 +6,4 @@ public class CreateEventDTO {
     private String dateTime;
     private String cep;
 
-    public String getEventName() {
-        return eventName;
-    }
-
-    public void setEventName(String eventName) {
-        this.eventName = eventName;
-    }
-
-    public String getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(String dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
 }
