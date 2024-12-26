ce7e9ad765008a29b0c8cc8b62f91856ae9db7cc
gustavodm2 <gmerlugodenardi@gmail.com>
2024-12-26 15:18:14 -0300

    feat: add function to sort in repository

--- ms-event-manager/src/main/java/events/repositories/EventRepository.java
+++ ms-event-manager/src/main/java/events/repositories/EventRepository.java
@@ -1,11 +1,15 @@
 package events.repositories;
 
 import events.entities.Event;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import java.util.List;
+
 @EnableMongoRepositories
 public interface EventRepository extends MongoRepository<Event, String> {
+    List<Event> findAll(Sort sort);
 
 }
