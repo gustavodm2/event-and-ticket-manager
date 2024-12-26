3460c122b499784983b0ddaf6332f5bbb05726ea
gustavodm2 <gmerlugodenardi@gmail.com>
2024-12-26 09:41:19 -0300

    feat: get all events endpoint

--- ms-event-manager/src/main/java/events/controllers/EventController.java
+++ ms-event-manager/src/main/java/events/controllers/EventController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/events")
 public class EventController {
@@ -19,6 +21,15 @@ public class EventController {
     @PostMapping
     public ResponseEntity<Event> createEvent(@RequestBody Event event) {
         Event createdEvent = eventService.createEvent(event);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdEvent);
+        return ResponseEntity.ok(createdEvent);
     }
+
+    @GetMapping
+    public ResponseEntity<List<Event>> getAllEvents(){
+        List<Event> events = eventService.getAllEvents();
+        return ResponseEntity.ok(events);
+    }
+
+
+
 }
