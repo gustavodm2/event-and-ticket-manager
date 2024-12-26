fbda560bb4ad2f78baab63833312c9de39e238e3
gustavodm2 <gmerlugodenardi@gmail.com>
2024-12-26 18:57:21 -0300

    fix: minor fix

--- ms-event-manager/src/main/java/events/entities/DTOs/CreateEventDTO.java
+++ ms-event-manager/src/main/java/events/entities/DTOs/CreateEventDTO.java
@@ -1,5 +1,12 @@
 package events.entities.DTOs;
 
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateEventDTO {
 
     private String eventName;
--- ms-event-manager/src/main/java/events/entities/DTOs/UpdateEventDTO.java
+++ ms-event-manager/src/main/java/events/entities/DTOs/UpdateEventDTO.java
@@ -1,5 +1,12 @@
 package events.entities.DTOs;
 
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdateEventDTO {
 
     public String eventName;
