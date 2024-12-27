49b1ae34daa4f216527d43a80bccd904b1737f4d
gustavodm2 <gmerlugodenardi@gmail.com>
2024-12-27 16:03:40 -0300

    feat: feign client event client

--- ms-ticket-manager/src/main/java/tickets/ms_ticket_manager/clients/EventClient.java
+++ ms-ticket-manager/src/main/java/tickets/ms_ticket_manager/clients/EventClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import tickets.ms_ticket_manager.entities.DTOs.EventDTO;
 
-@FeignClient(name = "ms-event-manager", url = "https://localhost:8080")
+@FeignClient(name = "ms-event-manager", url = "http://localhost:8080/")
 public interface EventClient {
 
     @GetMapping("/events/{id}")
