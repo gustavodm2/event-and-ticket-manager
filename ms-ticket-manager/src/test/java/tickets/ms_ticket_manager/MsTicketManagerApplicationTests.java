2484d2d719fa3a98b62358f9af3104d6fad9aded
gustavodm2 <gmerlugodenardi@gmail.com>
2024-12-27 13:10:09 -0300

    feat: add ticket manager project

---
+++ ms-ticket-manager/src/test/java/tickets/ms_ticket_manager/MsTicketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tickets.ms_ticket_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTicketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
