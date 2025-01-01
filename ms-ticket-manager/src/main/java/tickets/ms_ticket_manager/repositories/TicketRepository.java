97724b38623702f53f35d9b64f26245bb7d37909
gustavodm2 <gmerlugodenardi@gmail.com>
2025-01-01 15:06:14 -0300

    feat: create get tickets by event id function in service

--- ms-ticket-manager/src/main/java/tickets/ms_ticket_manager/repositories/TicketRepository.java
+++ ms-ticket-manager/src/main/java/tickets/ms_ticket_manager/repositories/TicketRepository.java
@@ -3,5 +3,8 @@ package tickets.ms_ticket_manager.repositories;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import tickets.ms_ticket_manager.entities.Ticket;
 
+import java.util.List;
+
 public interface TicketRepository extends MongoRepository<Ticket, String> {
+    List<Ticket> findByEventId(String eventId);
 }
