eef4038bfb6d11e279f72aa5cf83098274602353
gustavodm2 <gmerlugodenardi@gmail.com>
2025-01-05 11:04:03 -0300

    feat: swagger config

---
+++ ms-event-manager/src/main/java/events/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package events.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Event Management API")
+                        .version("1.0.0")
+                        .description("API para gerenciamento de eventos, incluindo criação, atualização, exclusão e consulta de eventos."));
+    }
+}
