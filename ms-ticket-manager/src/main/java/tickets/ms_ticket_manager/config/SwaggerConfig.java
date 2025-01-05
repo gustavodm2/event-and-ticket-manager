032e9f62792a6f27d56baabb4b8acec6802c9484
gustavodm2 <gmerlugodenardi@gmail.com>
2025-01-05 11:29:39 -0300

    feat: swagger config (ms-ticket)

--- ms-ticket-manager/src/main/java/tickets/ms_ticket_manager/config/SwaggerConfig.java
+++ ms-ticket-manager/src/main/java/tickets/ms_ticket_manager/config/SwaggerConfig.java
@@ -1,9 +1,9 @@
-package events.config;
+package tickets.ms_ticket_manager.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
